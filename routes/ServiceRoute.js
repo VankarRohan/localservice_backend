f25c9425a39d0792bee5d20e1b38a048178162cc
VankarRohan <vankarrohan56@gmail.com>
2024-03-07 18:18:54 +0530

    initial commit

---
+++ routes/ServiceRoute.js
@@ -0,0 +1,10 @@
+const routes = require('express').Router()
+const servicecontroller = require('../controllers/ServiceController')
+
+routes.post("/services",servicecontroller.createservice)
+routes.get("/services",servicecontroller.getservice)
+routes.get("/services/:id",servicecontroller.getserviceById)
+routes.delete("/services/:id",servicecontroller.deleteservice)
+routes.put("/services/:id",servicecontroller.updateservice)
+
+module.exports = routes
